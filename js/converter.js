db9ca4da6e7d7407409e5be7c5721153830cea5d
Lalith-Konda-101 <lalithkonda24k@gmail.com>
2026-08-02 18:59:15 +0530

    Update converter.js

--- js/converter.js
+++ js/converter.js
@@ -1,1 +1,102 @@
+// =========================
+// HEX -> RGB
+// =========================
 
+export function hexToRgb(hex) {
+
+    hex = hex.replace("#", "");
+
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    return { r, g, b };
+}
+
+// =========================
+// RGB -> HEX
+// =========================
+
+export function rgbToHex(r, g, b) {
+
+    return "#" +
+        [r, g, b]
+            .map(value => value.toString(16).padStart(2, "0"))
+            .join("")
+            .toUpperCase();
+}
+
+// =========================
+// RGB -> Decimal
+// =========================
+
+export function rgbToDecimal(r, g, b) {
+
+    return (r << 16) + (g << 8) + b;
+}
+
+// =========================
+// Decimal -> RGB
+// =========================
+
+export function decimalToRgb(decimal) {
+
+    decimal = Number(decimal);
+
+    return {
+        r: (decimal >> 16) & 255,
+        g: (decimal >> 8) & 255,
+        b: decimal & 255
+    };
+}
+
+// =========================
+// RGB -> HSL
+// =========================
+
+export function rgbToHsl(r, g, b) {
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+
+    let h, s;
+    const l = (max + min) / 2;
+
+    if (max === min) {
+        h = 0;
+        s = 0;
+    } else {
+
+        const d = max - min;
+
+        s = l > 0.5
+            ? d / (2 - max - min)
+            : d / (max + min);
+
+        switch (max) {
+
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+
+            case g:
+                h = (b - r) / d + 2;
+                break;
+
+            default:
+                h = (r - g) / d + 4;
+        }
+
+        h /= 6;
+    }
+
+    return {
+        h: Math.round(h * 360),
+        s: Math.round(s * 100),
+        l: Math.round(l * 100)
+    };
+}
