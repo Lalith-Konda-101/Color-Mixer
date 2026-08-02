8b58b54714141313927a8f3ee8e5c90d3f425b4c
Lalith-Konda-101 <lalithkonda24k@gmail.com>
2026-08-02 19:02:47 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 import { detectFormat } from "./parser.js";
+import { initPreview } from "./preview.js";
+import { initUI } from "./ui.js";
+
+initPreview(document.getElementById("color-preview"));
+
+initUI({
+    hex: document.getElementById("hex-output"),
+    rgb: document.getElementById("rgb-output"),
+    decimal: document.getElementById("decimal-output")
+});
 
 const inputField = document.getElementById("color-input");
 const inputType = document.getElementById("input-type");
