ef22c133ee615fda1b976e7aaa3e91ba00ee3216
Lalith-Konda-101 <lalithkonda24k@gmail.com>
2026-08-02 18:56:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+import { detectFormat } from "./parser.js";
 
+const inputField = document.getElementById("color-input");
+const inputType = document.getElementById("input-type");
+
+function initialize() {
+    inputField.addEventListener("input", handleInput);
+
+    console.log("✅ Color Mixer initialized.");
+}
+
+function handleInput() {
+    const value = inputField.value.trim();
+
+    const result = detectFormat(value);
+
+    console.log(result);
+
+    // Later:
+    // validate(result);
+    // convert(result);
+    // updateUI(result);
+}
+
+initialize();
