4805ff8475960331b84c7b2bc62db153adca8a94
Lalith-Konda-101 <lalithkonda24k@gmail.com>
2026-08-02 18:58:34 +0530

    Update validator.js

--- js/validator.js
+++ js/validator.js
@@ -1,1 +1,33 @@
+export function validateColor(type, value) {
 
+    switch (type) {
+
+        case "hex":
+            return /^#?[0-9A-Fa-f]{6}$/.test(value);
+
+        case "rgb": {
+            const numbers = value
+                .replace(/rgb|\(|\)|\s/g, "")
+                .split(",")
+                .map(Number);
+
+            return (
+                numbers.length === 3 &&
+                numbers.every(n => Number.isInteger(n) && n >= 0 && n <= 255)
+            );
+        }
+
+        case "decimal": {
+            const number = Number(value);
+
+            return (
+                Number.isInteger(number) &&
+                number >= 0 &&
+                number <= 16777215
+            );
+        }
+
+        default:
+            return false;
+    }
+}
